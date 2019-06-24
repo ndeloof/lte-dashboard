3372d1e600962dd74932a7e936d686517dc4f38c
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-06-24 11:07:19 +0200

    Show signal metrics in a loop
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, session, Tray, ipcMain} = require('electron')
 
 const sha256 = require('js-sha256').sha256
-const Base64 = require('js-base64').Base64;
-const axios = require('axios');
-const parser = require('fast-xml-parser');
+const Base64 = require('js-base64').Base64
+const axios = require('axios')
+const parser = require('fast-xml-parser')
+const fs = require('fs')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, connectWindow, tray
 
 let cookie
 
-const password = process.env.PASSWORD;
+let configuration = {
+  url: 'http://192.168.8.1',
+  login: 'admin',
+  password: ''
+}
+
+let scheduled
 
 const http = axios.create({
-  baseURL: 'http://192.168.1.1/api/',
   timeout: 10000,
   transformRequest: [function (data, headers) {
-    headers.Accept = "application/xml";
+    headers.Accept = "application/xml"
     if (cookie) {
-      headers.Cookie = cookie;
+      headers.Cookie = cookie
     }
-    return data;
+    return data
   }],
-});
+})
+
+
+function createTray() {
+  tray = new Tray('trayTemplate@2x.png')
+  tray.setToolTip('LTE dashboard')
+  tray.on('click', e => {
+    mainWindow.show()
+  })
+}
 
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
+
+  createTray()
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
@@ -38,45 +55,119 @@ function createWindow () {
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  loadConfiguration()
 
-  http.get('webserver/SesTokInfo')
+  collectMetrics()
+
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+    if (scheduled) {
+      clearTimeout(scheduled)
+    }
+  })
+}
+
+// Load persisted router configuration
+function loadConfiguration() {
+  const cfg = app.getPath('appData')+'/lte-dashboard/config.json'
+  try {
+    configuration = JSON.parse(fs.readFileSync(cfg))
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+// Show configuration modal to get user fix the configuration
+function configure() {
+  connectWindow = new BrowserWindow({
+    width: 600, height: 300,
+    webPreferences: { nodeIntegration: true },
+    parent: mainWindow,
+    modal: true,
+  })
+
+  connectWindow.loadFile('connect.html')
+  mainWindow.show()
+
+  ipcMain.on('connection-details', (event, data) => {
+    configuration = data
+    const cfg = app.getPath('appData')+'/lte-dashboard/config.json'
+    fs.writeFileSync(cfg, JSON.stringify(configuration))
+    console.log(configuration)
+    connectWindow.destroy()
+    collectMetrics()
+  })
+}
+
+// Collect router metrics
+function collectMetrics() {
+  http.get(configuration.url+'/api/net/net-mode').then( (response) => {
+    const xml = response.data
+    var data = parser.parse(xml)
+    console.log(data)
+    mainWindow.webContents.send('net-mode', data.response)
+  })
+  http.get(configuration.url+'/api/device/signal').then( (response) => {
+    const xml = response.data
+    var data = parser.parse(xml)
+    if (data.error) {
+      console.log(data.error.code)
+      login()
+    } else {
+      console.log(data)
+      mainWindow.webContents.send('metrics', data.response)
+      // re-schedule
+      scheduled = setTimeout(collectMetrics, 3000)
+    }
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+    configure()
+  })
+}
+
+
+function login() {
+  http.get(configuration.url+'/api/webserver/SesTokInfo')
     .then(function (response) {
-      const xml = response.data;
+      const xml = response.data
       var data = parser.parse(xml)
-      session = data.response.SesInfo;
-      const token = data.response.TokInfo;
-      cookie = 'SessionId='+session
+      const token = data.response.TokInfo
+      const sessionId = data.response.SesInfo
+      cookie = 'SessionId='+sessionId
 
-      const hash = Base64.encode(sha256("admin"+Base64.encode(sha256(password))+token));
+      const hash = Base64.encode(sha256(configuration.login+Base64.encode(sha256(configuration.password))+token))
       const request = {
         "request": {
           "Username":"admin",
           "Password": hash,
           "password_type": 4
         }
-      };
+      }
       const login = new parser.j2xParser({}).parse(request)
-      http({method:'post',
-            url:'user/login',
+      http({method: 'post',
+            url: configuration.url+'/api/user/login',
             headers: {'__RequestVerificationToken' : token},
             data: login
       }).then(function(response) {
         const setCookie = response.headers['set-cookie']
-        // Cookie get replaced after successful authentication
+        // Cookie get replaced after a successful authentication
         if (setCookie) {
           cookie = setCookie
-        }
+        }        
 
-        http.get('device/signal').then( console.log )
+        const xml = response.data
+        var data = parser.parse(xml)
+        if (data.error) {
+          configure()
+        } else {
+          collectMetrics()
+        }
       })
     })
-
-  // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
 }
 
 // Electron `app` is ready
@@ -84,7 +175,8 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  tray.destroy()
+  if (process.platform !== 'darwin') app.quit()  
 })
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { ipcRenderer } = require('electron')
+
+console.log('hello!')
+
+ipcRenderer.on('net-mode', (event, data) => {
+    document.getElementById('NetworkMode').innerText = data.NetworkMode
+    document.getElementById('NetworkBand').innerText = data.NetworkBand
+    document.getElementById('LTEBand').innerText = data.LTEBand
+})
+
+ipcRenderer.on('metrics', (event, data) => {
+    document.getElementById('rsrq').innerText = data.rsrq
+    document.getElementById('rsrp').innerText = data.rsrp
+    document.getElementById('rssi').innerText = data.rssi
+    document.getElementById('sinr').innerText = data.sinr
+    document.getElementById('earfcn').innerText = data.earfcn
+})
