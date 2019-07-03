67ab8923f94776625306c536c6926ef5ae568bb3
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-07-03 10:24:16 +0200

    Baby step learning React
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- app.js
+++ app.js
@@ -3,16 +3,20 @@
 // All of the Node.js APIs are available in this process.
 
 const { ipcRenderer } = require('electron')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 console.log('hello!')
 
 ipcRenderer.on('net-mode', (event, data) => {
+    console.log(data)
     document.getElementById('NetworkMode').innerText = data.NetworkMode
     document.getElementById('NetworkBand').innerText = data.NetworkBand
     document.getElementById('LTEBand').innerText = data.LTEBand
 })
 
 ipcRenderer.on('metrics', (event, data) => {
+    console.log(data)
     setMetric('rsrq', data.rsrq, 'dB', -10, -15, -20)
     setMetric('rsrp', data.rsrp, 'dBm', -80, -90, -100)
     setMetric('rssi', data.rssi, 'dBm', -80, -90, -100)
@@ -21,7 +25,7 @@ ipcRenderer.on('metrics', (event, data) => {
 })
 
 function setMetric(name, value, unit, excellent, good, acceptable) {
-    target = document.getElementById(name)
+    const target = document.getElementById(name)
     target.innerText = value + ' ' + unit
     if (value >= excellent) {
         target.style='color:limegreen'
@@ -32,4 +36,37 @@ function setMetric(name, value, unit, excellent, good, acceptable) {
     } else {
         target.style='color:red'
     }
+}
+
+ReactDOM.render(<Metrics/>, document.getElementById('metrics'))
+
+function Metrics() {
+  return (
+    <tbody>
+      <tr>
+        <td>NetworkMode</td><td id="NetworkMode"></td>
+      </tr>
+      <tr>
+        <td>NetworkBand</td><td id="NetworkBand"></td>
+      </tr>
+      <tr>
+        <td>LTEBand</td><td id="LTEBand"></td>
+      </tr>
+      <tr>
+        <td title="Reference signal Receive Quality">rsrq</td><td id="rsrq"></td>
+      </tr>
+      <tr>
+        <td title="Reference signal Receive Power">rsrp</td><td id="rsrp"></td>
+      </tr>
+      <tr>
+        <td title="Reference signal Strenght Indication">rssi</td><td id="rssi"></td>
+      </tr>
+      <tr>
+        <td title="Signal to Interference + Noise Ratio">sinr</td><td id="sinr"></td>
+      </tr>
+      <tr>
+        <td title="Absolute Radio Frequency Channel Number">earfcn</td><td id="earfcn"></td>
+      </tr>
+    </tbody>
+  )
 }
